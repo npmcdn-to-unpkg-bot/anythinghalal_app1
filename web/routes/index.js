65e61b4e42c93732bf97b53d2baa292b6deaac52
yazid <flight846@gmail.com>
2016-08-11 11:18:13 +0800

    Add cors in views/routes

--- web/routes/index.js
+++ web/routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express')
 var router = express.Router()
 
+router.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Methods', 'POST, PUT, GET, DELETE, OPTIONS')
+  next()
+})
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index')
