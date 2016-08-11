3cb23176bd39567aa2884ab3a4d0699fbb7310d7
yazid <flight846@gmail.com>
2016-08-11 14:00:49 +0800

    Add category controller for heroku errors

--- api/models/category.js
+++ api/models/category.js
@@ -5,4 +5,13 @@ var CategorySchema = new Schema({
   category: String
 })
 
+CategorySchema.pre('save', function (next) {
+  var now = new Date()
+  this.updated_at = now
+  if (!this.created_at) {
+    this.created_at = now
+  }
+  next()
+})
+
 module.exports = mongoose.model('Category', CategorySchema)
